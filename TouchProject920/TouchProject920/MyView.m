5cdf6fa518633847f7d736e5a1d387a0d1285faf
AmberWhiteSky <amberwhitesky@163.com>
2015-09-20 21:41:47 +0800

    UITouch *touch =[touches anyObject];     //短时间内按的次数  判断用户短时间内点击了多少次     NSLog(@"%lu",touch.tapCount);

--- TouchProject920/TouchProject920/MyView.m
+++ TouchProject920/TouchProject920/MyView.m
@@ -13,18 +13,34 @@
 
 // 触摸开始
 -(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    NSLog(@"touchesBegan");
+//    NSLog(@"touchesBegan");
+    UITouch *touch =[touches anyObject];
+    //短时间内按的次数  判断用户短时间内点击了多少次
+    NSLog(@"%lu",touch.tapCount);
 
 }
 //触摸ing
 -(void)touchesMoved:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     NSLog(@"touchesMoved");
+    
+//    UITouch  *touch = [touches  anyObject];
+//    //当前触摸点
+//    CGPoint  currentpoint =[touch locationInView:self];
+//    //上一个触摸点
+//    CGPoint previouspoint =[touch previousLocationInView:self];
+//    
+//    //修改当前view的位置（中点）
+//    CGPoint center =self.center;
+//    
+//    center.x += currentpoint.x-previouspoint.x;
+//    center.y +=currentpoint.y-previouspoint.y;
+//    self.center =center;
 
 
 }
 //触摸结束
 -(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
-    NSLog(@"touchesEnded");
+//    NSLog(@"touchesEnded");
 
 }
 
